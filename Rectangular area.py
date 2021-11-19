82cdc42092dab058a02d57061da69d95915d47d0
krishnadesikch <77722912+krishnadesikch@users.noreply.github.com>
2021-11-19 11:58:10 +0530

    Create Rectangular area.py
    You are given a rectangle in a plane. The coordinates of one of its diagonals are provided to you. You have to print the total area of the rectangle.
    The coordinates of the rectangle are provided as four integral values: x1, y1, x2, y2. It is given that x1 < x2 and y1 < y2.

---
+++ Rectangular area.py
@@ -0,0 +1,9 @@
+x1 = int(input())
+y1 = int(input())
+x2 = int(input())
+y2 = int(input())
+if(x1>=1 and x1<=10):
+    if(y1>=1 and y1<=10):
+        if(x2>=1 and x2<=10):
+            if(y2>=1 and y2<=10):
+                print((x2-x1)*(y2-y1))
